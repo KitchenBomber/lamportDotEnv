b4da5bf05d0af2cdf4af9300d7402ad98fb7f00b
Alan Henderson <mr.alan.henderson@gmail.com>
2020-05-14 10:08:08 -0500

    GIPHY KEY is updating

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
 const express = require( 'express' );
 const app = express();
 const axios = require( 'axios' );
+const dotenv = require( 'dotenv' )
+
+dotenv.config();
+
+//test to see if GIPHY API key is available through dotenv
+console.log('GIPHY_API_KEY', process.env.GIPHY_API_KEY );
 
 const port = process.env.PORT || 5000;
 
